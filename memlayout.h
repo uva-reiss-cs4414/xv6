a6c8722bd7c4a31ccebff3b007eb742ea9939eee
cr4bd <cr4bd@virginia.edu>
2019-12-19 11:09:13 -0500

    comment/panic message adjustments

--- memlayout.h
+++ memlayout.h
@@ -10,11 +10,18 @@
 
 #ifndef __ASSEMBLER__
 
+// I changed V2P and P2V from macros into functions in order to make sure
+// errors occur if pointers are passed to P2V and make sure errors occur
+// when user addresses are passed to V2P. - Charles Reiss
+
 // Convert kernel virtual address to physical address
 static inline uint V2P(void *a) {
+    // define panic() here because memlayout.h is included before defs.h
     extern void panic(char*) __attribute__((noreturn));
     if (a < (void*) KERNBASE)
-        panic("V2P on address < KERNBASE (not a kernel virtual address)");
+        panic("V2P on address < KERNBASE "
+              "(not a kernel virtual address; consider walking page "
+              "table to determine physical address of a user virtual address)");
     return (uint)a - KERNBASE;
 }
 
